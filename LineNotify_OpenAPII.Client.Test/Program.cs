882c035e70479650753d14750228f4faa5ca0b17
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-02-15 11:29:06 +0800

    部分模組去耦

--- LineNotify_OpenAPII.Client.Test/Program.cs
+++ LineNotify_OpenAPII.Client.Test/Program.cs
@@ -8,13 +8,19 @@
             try
             {
                 {
+                    Console.Write("input api_url：");
+                    var api_url = Console.ReadLine();
+                    if (api_url is null) throw new Exception("api_url is null");
+
                     Console.Write("input token：");
                     var token = Console.ReadLine();
+                    if (token is null) throw new Exception("token is null");
+
                     Console.Write("message：");
                     var message = Console.ReadLine();
+                    if (message is null) throw new Exception("message is null");
 
-                    var Result = new api().notify(token, message);
-                    if (Result is null) throw new Exception("result is null");
+                    var Result = new api().notify(api_url, token, message);
 
                     Console.WriteLine($"GetResult：status={Result.status};message={Result.message}");
                 }
